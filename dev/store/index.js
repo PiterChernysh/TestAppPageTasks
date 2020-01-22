3449483d05585ad74a4911e863063081a4cc388f
Piter <piter.dev.nodejs@gmail.com>
2020-01-22 13:00:46 +0200

    Finished project

--- dev/store/index.js
+++ dev/store/index.js
@@ -7,8 +7,7 @@ const store = createStore(
   reducer,
   compose(
     applyMiddleware(thunk)
-    // ,
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    
   )
 );
 
